97fa74319bf6d297bc4ed01c0078be468ac87938
kiki <22114129@students.latrobe.edu.au>
2024-08-31 17:56:53 +1000

    Adding fortune cookie message

--- server.js
+++ server.js
@@ -6,7 +6,15 @@ const app = express();
 
 // Specify how to respond to GET / 
 app.get('/', (req, res) => { 
-    res.send('Hello World!'); 
+    
+     // Run the system `fortune` command and respond with the message 
+     child_process.exec('fortune', (error, message) => { 
+        if(error === null) { 
+            res.send(message); 
+        } else { 
+            res.send('Error: ' + error); 
+        } 
+    }) 
 }); 
 
 // Start listening for HTTP requests on port 3000 
